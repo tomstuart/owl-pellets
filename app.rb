111f89329406a91024fdcf198f3c710ae66d209a
Tom Stuart <tom@codon.com>
2016-06-09 17:02:31 +0100

    Add tweets to author timeline view

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'json'
 require 'time'
+require 'redis'
+
+$redis = Redis.new
 
 def event_log
   @event_log ||= Logger.new(File.join(File.dirname(__FILE__), 'log.txt'))
@@ -17,6 +20,8 @@ def log(data)
 end
 
 get '/user/:user_id' do
+  tweet_ids = $redis.lrange("author_timeline:#{params[:user_id]}", 0, -1)
+  @tweets = tweet_ids.map { |tweet_id| $redis.hgetall("tweet:#{tweet_id}") }
   erb :timeline
 end
 
